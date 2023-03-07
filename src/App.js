4dab294d2b3f576c1fbdfcda9e47777f3c2ff743
crystalmanner <44223659+crystalmanner@users.noreply.github.com>
2023-03-07 16:11:28 -0500

    implement mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Play from "./components/Play";
+import Game from "./components/Game";
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const [myChoice, setMyChoice] = useState("");
+  const [score, setScore] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <Header score={score} />
+        <Routes>
+          <Route path="/" element={<Play setMyChoice={setMyChoice} />} />
+          <Route path="/game" element={<Game myChoice={myChoice} score={score} setScore={setScore} />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <div className="attribution">
+          Skill Test Shifumi Game
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
